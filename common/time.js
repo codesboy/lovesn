356749b4f3a8de0db792c6a37e1aad9fa022dc21
codesboy <565195693@qq.com>
2018-01-31 19:14:15 +0800

    init

---
+++ common/time.js
@@ -0,0 +1,29 @@
+function siteTime() {
+
+    var seconds = 1000;
+    var minutes = seconds * 60;
+    var hours = minutes * 60;
+    var days = hours * 24;
+    var years = days * 365;
+    // var today = new Date();
+    // var todayYear = today.getFullYear();
+    // var todayMonth = today.getMonth();
+    // var todayDate = today.getDate();
+    // var todayHour = today.getHours();
+    // var todayMinute = today.getMinutes();
+    // var todaySecond = today.getSeconds();
+    var nowTime = new Date().getTime();//当前时间戳
+    var loveBeginTime = new Date('2018 / 01 / 19').getTime();//恋爱开始时间戳
+    var diff = nowTime - loveBeginTime;//恋爱的时长
+    var diffYears = Math.floor(diff / years);
+    var diffDays = Math.floor((diff / days) - diffYears * 365);
+    var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
+    var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
+    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
+    if (diffSeconds.toString().length == 1) {
+        diffSeconds = '0' + diffSeconds;
+    }
+    return (diffYears + "年" + diffDays + "天" + diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒");
+}
+
+module.exports.time =siteTime;
---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+//index.js
+const time = require('../../common/time.js');
+
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        loveTime:'',
+        loveShow:''
+    },
+    onLoad:function(){
+        var _this = this;
+        wx.request({
+            url: app.globalData.baseUrl+'showlove',
+            method:'POST',
+            success:function(res){
+                console.log(res.data)
+                _this.setData({
+                    loveShow:res.data.show
+                })
+            },
+            fail:function(e){
+                console.log(e.errMsg)
+            }
+        })
+    },
+    onShow: function () {
+        // console.log(time.time())
+        // siteTime()
+        this.setData({
+            loveTime: time.time()
+        });
+        var _this = this;
+        setInterval(function(){
+            _this.setData({
+                loveTime: time.time()
+            })
+        }, 1000);
+    }
+})
